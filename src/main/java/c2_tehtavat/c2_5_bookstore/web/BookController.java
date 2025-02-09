802926529c012b0843aa57ffa9476e45f6a2a772
Tiriele <147039063+Tiriele@users.noreply.github.com>
2025-02-09 18:50:41 +0200

    3.2 tehty

--- src/main/java/c2_tehtavat/c2_5_bookstore/web/BookController.java
+++ src/main/java/c2_tehtavat/c2_5_bookstore/web/BookController.java
@@ -1,15 +1,28 @@
 package c2_tehtavat.c2_5_bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import c2_tehtavat.c2_5_bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @GetMapping("/index")
-    public String getBooks() {
+    public String getIndex() {
         
         return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String getBooklist(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
     }
 
 }
