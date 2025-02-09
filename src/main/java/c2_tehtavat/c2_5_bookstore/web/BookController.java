1f9407ab2ee6f7d5772112e92903c3e5739cfdb9
Tiriele <147039063+Tiriele@users.noreply.github.com>
2025-02-09 20:10:42 +0200

    3.3 tehty

--- src/main/java/c2_tehtavat/c2_5_bookstore/web/BookController.java
+++ src/main/java/c2_tehtavat/c2_5_bookstore/web/BookController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import c2_tehtavat.c2_5_bookstore.domain.Book;
 import c2_tehtavat.c2_5_bookstore.domain.BookRepository;
 
 @Controller
@@ -23,6 +26,24 @@ public class BookController {
     public String getBooklist(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        bookRepository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:booklist";
     }
 
 }
