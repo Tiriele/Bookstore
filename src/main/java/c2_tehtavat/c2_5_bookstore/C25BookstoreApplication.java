9da8b630b21f79db161e4045e7bbaa9a7c9d0be9
Tiriele <147039063+Tiriele@users.noreply.github.com>
2025-02-21 15:25:23 +0200

    CommandLineRunner korjaus

--- src/main/java/c2_tehtavat/c2_5_bookstore/C25BookstoreApplication.java
+++ src/main/java/c2_tehtavat/c2_5_bookstore/C25BookstoreApplication.java
@@ -1,5 +1,7 @@
 package c2_tehtavat.c2_5_bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,7 @@ import c2_tehtavat.c2_5_bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class C25BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(C25BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(C25BookstoreApplication.class, args);
@@ -18,14 +21,15 @@ public class C25BookstoreApplication {
 	@Bean
 	public CommandLineRunner bookDemo(BookRepository bookrepository) {
 		return (args) -> {
-			Book book1 = new Book("nimi1", "kirjailija1", 0, "isbn1", 0);
-			Book book2 = new Book("nimi2", "kirjailija2", 0, "isbn2", 0);
-			Book book3 = new Book("nimi3", "kirjailija3", 0, "isbn3", 0);
+			log.info("save a couple of books");
+			bookrepository.save(new Book("nimi1", "kirjailija1", 0, "isbn1", 0));
+			bookrepository.save(new Book("nimi2", "kirjailija2", 0, "isbn2", 0));
+			bookrepository.save(new Book("nimi3", "kirjailija3", 0, "isbn3", 0));
 
-			bookrepository.save(book1);
-			bookrepository.save(book2);
-			bookrepository.save(book3);
-
+			log.info("fetch all books");
+			for (Book book : bookrepository.findAll()) {
+ 			log.info(book.toString());
+}
 			};
 		}
 
