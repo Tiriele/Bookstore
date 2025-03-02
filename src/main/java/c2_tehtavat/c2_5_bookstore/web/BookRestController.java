97f40f2bc16f34ee515a378e2faccc10175fe265
Tiriele <147039063+Tiriele@users.noreply.github.com>
2025-03-02 19:54:43 +0200

    5.2 tehty?

---
+++ src/main/java/c2_tehtavat/c2_5_bookstore/web/BookRestController.java
@@ -0,0 +1,40 @@
+package c2_tehtavat.c2_5_bookstore.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import c2_tehtavat.c2_5_bookstore.domain.Book;
+import c2_tehtavat.c2_5_bookstore.domain.BookRepository;
+
+@CrossOrigin
+@Controller
+public class BookRestController {
+
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/books")
+    public @ResponseBody List<Book> getAllBooksRest() {
+        return (List<Book>) bookRepository.findAll();
+    }
+
+    @GetMapping("/books/{id}")
+    public @ResponseBody Optional<Book> getBookById (@PathVariable(name = "id") Long bookId) {
+        return bookRepository.findById(bookId);
+}
+
+    @PostMapping("/books")
+    public @ResponseBody Book addNewBookRest(@RequestBody Book newBook) {
+        return bookRepository.save(newBook);
+    }
+}
